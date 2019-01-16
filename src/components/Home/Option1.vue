cd1d75426ac5374e692c7ddd11a295ef56eb73cb
Annie <banasikannie@gmail.com>
2019-01-16 01:01:06 +0000

    option 2 design added

---
+++ src/components/Home/Option1.vue
@@ -0,0 +1,76 @@
+<template>
+<b-card no-body class="ml-2 strip-legs-option ">
+    <div slot="header">
+        Strip > Legs
+        <div class="card-header-actions">
+            <b-link href="#" class="card-header-action btn-setting">
+                <i class="fa fa-plus"></i>
+            </b-link>
+        </div>
+    </div>
+    <b-card-body class="p-0">
+        <b-list-group flush>
+            <b-list-group-item>
+                <b-form-select name="optionClass" :plain="true" :options="['Vanilla', 'Cash Deals']" value="Vanilla">
+                </b-form-select>
+            </b-list-group-item>
+            <b-list-group-item>
+                <b-form-select name="callPutOption" :plain="true" :options="['GBP Put', 'GBP Call']" value="GBP Put">
+                </b-form-select>
+            </b-list-group-item>
+            <b-list-group-item>
+                <b-form-input name="strike" type="text" size="sm" placeholder="1.3000"></b-form-input>
+            </b-list-group-item>
+            <b-list-group-item>&nbsp;</b-list-group-item>
+            <b-list-group-item>&nbsp;</b-list-group-item>
+            <b-list-group-item>
+                <b-form-group class="m-0" label="Strip begin date" label-for="strip_begin_date" :label-cols="5" label-size="sm" :horizontal="true">
+                    <b-form-input type="date" size="sm" name="strip_begin_date"></b-form-input>
+                </b-form-group>
+            </b-list-group-item>
+            <b-list-group-item>
+                <b-form-group class="m-0" label="Strip end date" label-for="strip_end_date" :label-cols="5" label-size="sm" :horizontal="true">
+                    <b-form-input type="date" size="sm" name="strip_end_date"></b-form-input>
+                </b-form-group>
+            </b-list-group-item>
+            <b-list-group-item>
+                <b-form-group class="m-0" label="Expiries" label-for="expiries" label-size="sm" :label-cols="5" :horizontal="true">
+                    <b-form-input name="expiries" type="text" size="sm" placeholder="6"></b-form-input>
+                </b-form-group>
+            </b-list-group-item>
+            <b-list-group-item>
+                <b-form-group class="m-0" label="Strip By" label-for="strip_by" :label-cols="5" label-size="sm" :horizontal="true">
+                    <b-form-select name="strip_by" :plain="true" :options="['Expiry']" value="Expiry">
+                    </b-form-select>
+                </b-form-group>
+            </b-list-group-item>
+            <b-list-group-item>
+                <b-button block variant="secondary" class="btn-pill" size="sm">Strip Details</b-button>
+            </b-list-group-item>
+            <b-list-group-item>
+                Leg Total National:
+            </b-list-group-item>
+            <b-list-group-item>
+                600,000
+            </b-list-group-item>
+            <b-list-group-item>
+                <b-form-group class="m-0" label="" label-for="nationalIn">
+                    <b-input-group>
+                        <b-form-input size="sm" name="nationalIn" type="text" value="100,000"></b-form-input>
+                        <b-input-group-append>
+                            <b-button size="sm" variant="primary">Buy</b-button>
+                        </b-input-group-append>
+                    </b-input-group>
+                </b-form-group>
+            </b-list-group-item>
+            <b-list-group-item>&nbsp;</b-list-group-item>
+        </b-list-group>
+    </b-card-body>
+</b-card>
+</template>
+
+<script>
+export default {
+    name: "Option1"
+};
+</script>
---
+++ src/components/Home/Option2.vue
@@ -0,0 +1,83 @@
+<template>
+<b-card no-body class="border-radius-0 strip-legs-option option2">
+    <div slot="header">
+        Strip > Legs
+        <div class="card-header-actions">
+            <b-link href="#" class="card-header-action btn-setting">
+                <i class="fa fa-plus"></i>
+            </b-link>
+        </div>
+    </div>
+    <b-card-body class="p-0">
+        <b-list-group flush>
+            <b-list-group-item>
+                <b-form-select name="optionClass" :plain="true" :options="['Knock In (pbki)']" disabled value="Knock In (pbki)">
+                </b-form-select>
+            </b-list-group-item>
+            <b-list-group-item>
+                <b-form-select name="callPutOption" :plain="true" :options="['GBP Call', 'GBP Put']" value="GBP Call">
+                </b-form-select>
+            </b-list-group-item>
+            <b-list-group-item>
+                <b-form-input name="strike" type="text" size="sm" placeholder="1.3000"></b-form-input>
+            </b-list-group-item>
+            <b-list-group-item>
+                <div class="row">
+                    <div class="col">
+                        <b-form-input name="trigger1" type="text" size="sm" placeholder="1.3000"></b-form-input>
+                    </div>
+                    <div class="col">
+                        <b-form-select name="trigger1_type" :plain="true" :options="['Above', 'Below']" value="Above">
+                        </b-form-select>
+                    </div>
+                </div>
+            </b-list-group-item>
+            <b-list-group-item>&nbsp;</b-list-group-item>
+            <b-list-group-item>
+                <b-form-group class="m-0" label="Strip begin date" label-for="strip_begin_date" :label-cols="5" label-size="sm" :horizontal="true">
+                    <b-form-input type="date" size="sm" name="strip_begin_date"></b-form-input>
+                </b-form-group>
+            </b-list-group-item>
+            <b-list-group-item>
+                <b-form-group class="m-0" label="Strip end date" label-for="strip_end_date" :label-cols="5" label-size="sm" :horizontal="true">
+                    <b-form-input type="date" size="sm" name="strip_end_date"></b-form-input>
+                </b-form-group>
+            </b-list-group-item>
+            <b-list-group-item>&nbsp;</b-list-group-item>
+            <b-list-group-item>
+                Trigger Start
+            </b-list-group-item>
+            <b-list-group-item>
+                <b-form-group class="m-0" label="" label-for="trigger_start_date" :label-cols="0" label-size="sm" :horizontal="true">
+                    <b-form-input type="date" size="sm" name="trigger_start_date"></b-form-input>
+                </b-form-group>
+            </b-list-group-item>
+            <b-list-group-item>
+                Trigger End
+            </b-list-group-item>
+            <b-list-group-item>
+                <b-form-group class="m-0" label="" label-for="trigger_end_date" :label-cols="0" label-size="sm" :horizontal="true">
+                    <b-form-input type="date" size="sm" name="trigger_end_date"></b-form-input>
+                </b-form-group>
+            </b-list-group-item>
+            <b-list-group-item>
+                <b-form-group class="m-0" label="" label-for="nationalIn">
+                    <b-input-group>
+                        <b-form-input size="sm" name="nationalIn" type="text" value="100,000"></b-form-input>
+                        <b-input-group-append>
+                            <b-button size="sm" variant="warning">Sell</b-button>
+                        </b-input-group-append>
+                    </b-input-group>
+                </b-form-group>
+            </b-list-group-item>
+            <b-list-group-item>&nbsp;</b-list-group-item>
+        </b-list-group>
+    </b-card-body>
+</b-card>
+</template>
+
+<script>
+export default {
+    name: "Option2"
+};
+</script>
